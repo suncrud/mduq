5602182e30f33b439a24fd654e12ce5cd640b902
sun lee <suncrud@users.noreply.github.com>
2025-10-26 15:27:58 +0900

    deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://suncrud.github.io/mduq',
+    base: 'mduq',
     output: 'static',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
